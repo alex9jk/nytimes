17b352d223f360cfb283143946808a35b2c4d628
Alex Kramer <alex9jk@gmail.com>
2020-06-23 17:23:37 -0400

    added nonfiction books and amazon link

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function xhr(getOrPost) {
+function xhr(getOrPost, listName) {
     return $.ajax({
         type: getOrPost,
-        url: `https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-fiction.json?api-key=lPPJjroH9qBXlRkTw1YEJwmpKa5FyR72`,
+        url: `https://api.nytimes.com/svc/books/v3/lists/current/${listName}.json?api-key=lPPJjroH9qBXlRkTw1YEJwmpKa5FyR72`,
         cache: false,
         async: true,
         dataType: 'json',
@@ -22,72 +22,140 @@ function xhr(getOrPost) {
     });
 
 }
-function getFiction(){
-    
+function createHtml(book, i) {
+    let column = document.createElement('div');
+    column.setAttribute('class', 'four wide column');
+    document.getElementsByClassName('ui grid')[0].append(column);
 
-    xhr('GET').done(function(json){
-        let h3 = document.createElement('h3');
-        let headerText = document.createTextNode("Fiction Combined Print and E-Books");
-        h3.appendChild(headerText);
-        document.getElementById('headerPrint').append(h3);
-        json.results.books.forEach(function(book, i){
-            let column = document.createElement('div');
-            column.setAttribute('class', 'four wide column');
-            document.getElementsByClassName('ui grid')[0].append(column);
+    let parent = document.createElement('div');
+    parent.setAttribute('class', 'ui card');
+    document.getElementsByClassName('four wide column')[i].append(parent);
 
-            let parent = document.createElement('div');
-            parent.setAttribute('class','ui card');
-            document.getElementsByClassName('four wide column')[i].append(parent);
+    let divImage = document.createElement('div');
+    divImage.setAttribute('class', 'image');
+    document.getElementsByClassName('ui card')[i].append(divImage);
 
-            let divImage = document.createElement('div');
-            divImage.setAttribute('class','image');
-            document.getElementsByClassName('ui card')[i].append(divImage);
+    let image = document.createElement('img');
+    image.setAttribute('src', book.book_image);
+    image.setAttribute('height', 250);
+    image.setAttribute('width', 166);
+    document.getElementsByClassName('image')[i].append(image);
 
-            let image = document.createElement('img');
-            image.setAttribute('src', book.book_image);
-            image.setAttribute('height',book.book_image_height);
-            image.setAttribute('width',book.book_image_width);
-            document.getElementsByClassName('image')[i].append(image);
+    let contentDiv = document.createElement('div');
+    contentDiv.setAttribute('class', 'contentDesc');
+    document.getElementsByClassName('ui card')[i].append(contentDiv);
 
-            let contentDiv = document.createElement('div');
-            contentDiv.setAttribute('class','contentDesc');
-            document.getElementsByClassName('ui card')[i].append(contentDiv);
+    let h3 = document.createElement('h3');
+    let titleText = document.createTextNode(book.rank + ". " + capitalize(book.title));
+    h3.appendChild(titleText);
+    document.getElementsByClassName('contentDesc')[i].append(h3);
 
-            let h3 = document.createElement('h3');
-            let titleText = document.createTextNode(book.rank +". " + capitalize(book.title));
-            h3.appendChild(titleText);
-            document.getElementsByClassName('contentDesc')[i].append(h3);
+    let metaDiv = document.createElement('div');
+    metaDiv.setAttribute('class', 'meta');
+    document.getElementsByClassName('contentDesc')[i].append(metaDiv);
 
-            let metaDiv = document.createElement('div');
-            metaDiv.setAttribute('class','meta');
-            document.getElementsByClassName('contentDesc')[i].append(metaDiv);
+    let span = document.createElement('span');
+    let spanText = document.createTextNode('Author: ' + book.author);
+    span.appendChild(spanText);
+    document.getElementsByClassName('meta')[i].append(span);
 
-            let span = document.createElement('span');
-            let spanText = document.createTextNode('Author: ' + book.author);
-            span.appendChild(spanText);
-            document.getElementsByClassName('meta')[i].append(span);
+    let descDiv = document.createElement('div');
+    descDiv.setAttribute('class', 'description');
+    let descText = document.createTextNode(book.description);
+    descDiv.appendChild(descText);
+    document.getElementsByClassName('contentDesc')[i].append(descDiv);
 
-            let descDiv = document.createElement('div');
-            descDiv.setAttribute('class','description');
-            let descText = document.createTextNode(book.description);
-            descDiv.appendChild(descText);
-            document.getElementsByClassName('contentDesc')[i].append(descDiv);
+    let linkToAmazon = document.createElement('a');
+    linkToAmazon.setAttribute('href',book.amazon_product_url);
+    let linkText = document.createTextNode('Buy on Amazon');
+    linkToAmazon.appendChild(linkText);
+    document.getElementsByClassName('contentDesc')[i].append(linkToAmazon);
+
+}
+function createHeader(title) {
+    let h3 = document.createElement('h3');
+    let headerText = document.createTextNode(title);
+    h3.appendChild(headerText);
+    document.getElementById('headerPrint').append(h3);
+}
+function getFictPaper() {
+    xhr('GET', 'trade-fiction-paperback').done(function (json) {
+        console.log(json);
+        createHeader("Paperback Trade Fiction");
+        json.results.books.forEach(function (book, i) {
+            createHtml(book, i);
+        })
+    })
+
+}
+function getFictHard() {
+    xhr('GET', 'hardcover-fiction').done(function (json) {
+        createHeader("Hardcover Fiction");
+        console.log(json);
+        json.results.books.forEach(function (book, i) {
+            createHtml(book, i);
+        })
+    })
 
 
-            
 
+}
+function getFiction() {
+    xhr('GET', 'combined-print-and-e-book-fiction').done(function (json) {
+        console.log(json);
+        createHeader("Fiction Combined Print and E-Books");
+        json.results.books.forEach(function (book, i) {
+            createHtml(book, i);
         })
     })
 }
-$( document ).ready(function() {
+
+function getEbookNon(){
+    xhr('GET', 'combined-print-and-e-book-nonfiction').done(function (json) {
+        createHeader("Combined Print & E-Book Non Fiction");
+        json.results.books.forEach(function (book, i) {
+            createHtml(book, i);
+        })
+    })
+}
+
+function getHardNon() {
+    xhr('GET', 'hardcover-nonfiction').done(function (json) {
+        createHeader("Hardcover Non Fiction");
+        json.results.books.forEach(function (book, i) {
+            createHtml(book, i);
+        })
+    })
+
+}
+function getPaperNon(){
+    xhr('GET', 'paperback-nonfiction').done(function (json) {
+        createHeader("Paperback Non Fiction");
+        json.results.books.forEach(function (book, i) {
+            createHtml(book, i);
+        })
+    })
+}
+
+function getMiscNon(){
+    xhr('GET', 'advice-how-to-and-miscellaneous').done(function (json) {
+        console.log(json);
+        createHeader("Advice, How To $ Miscellaneous");
+        json.results.books.forEach(function (book, i) {
+            createHtml(book, i);
+        })
+    })
+}
+
+$(document).ready(function () {
     $('.ui.dropdown').dropdown()
-;
+        ;
 });
 
 function capitalize(str) {
     var splitStr = str.toLowerCase().split(' ');
     for (var i = 0; i < splitStr.length; i++) {
-        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
     }
-    return splitStr.join(' '); 
+    return splitStr.join(' ');
 }
